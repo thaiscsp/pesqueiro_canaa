a3347b2b55bdccef263b22239f8f458806242176
thaiscsp <104219628+thaiscsp@users.noreply.github.com>
2022-10-19 11:52:50 -0300

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,29 +48,6 @@
 
 	<body>
 
-		<!-- Hero section -->
-		<div class="d-flex min-vh-100" lc-helper="background" style="background:url(https://raw.githubusercontent.com/thaiscsp/pesqueiro_canaa/13_10_22/public/images/cardapio.jpg)  center / cover no-repeat; background-color:#444; background-blend-mode: overlay;">
-    		<div class="align-self-center text-center text-light col-md-8 offset-md-2">
-        		<div class="lc-block mb-4">
-            		<div editable="rich">
-                		<h1 class="display-1 fw-bolder">Pesqueiro Canaã - Cardápio</h1>
-            		</div>
-        		</div>
-        		<div class="lc-block">
-            		<div editable="rich">
-                		<p class="lead">Confira todas as opções disponíveis!</p>
-            		</div>
-        		</div>
-        		<br>
-       			<div class="lc-block">
-            		<svg onclick="if (!document.querySelector('body').classList.contains('livecanvas-is-editing') ) this.closest('section').nextElementSibling.scrollIntoView({ behavior: 'smooth'  });" width="4em" height="4em" viewBox="0 0 16 16" class="text-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg" lc-helper="svg-icon">
-                		<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                		<path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"></path>
-            		</svg>
-        		</div>
-    		</div>
-		</div>
-
 		<!-- Navbar -->
         <div id="nav">
         	<ul class="nav justify-content-center">
