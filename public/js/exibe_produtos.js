9c31947cb88d1d3c9a83f40055d068f3f8f178d6
nandotheodoro <88256028+nandotheodoro@users.noreply.github.com>
2022-11-25 10:42:04 -0300

    Update exibe_produtos.js

--- public/js/exibe_produtos.js
+++ public/js/exibe_produtos.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                         '<center>' +
                             '<div class="col mb-4">' +
                                 '<div class="card">' +
-                                    '<img class="produto" src="https://www.dropbox.com/s/' +this.caminho_imagem+ '" class="rounded">' +
+                                    '<img class="produto" src="https://www.dropbox.com/s/' +this.caminho_imagem+ '" class="card-img-top">' +
                                     '<div class="rounded mx-auto d-block">' +
                                         '<h6 class="card-title">' +this.nome+ '</h6>' +
                                         '<p class="card-text">R$ ' +this.preco+ '</p>' +
