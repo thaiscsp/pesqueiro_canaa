dd1e9408e741f700ec399b2be4129c516c7a30c4
Renan-ss <116081257+Renan-ss@users.noreply.github.com>
2022-11-27 19:38:26 -0300

    Responsividade teste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -188,24 +188,4 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://pesqueiro-canaa.herokuapp.com/js/exibe_produtos.js"></script>
-
-
-<?php
-$mobile = FALSE;
-$user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
-foreach($user_agents as $user_agent){
-    if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
-        $mobile = TRUE;
-        $modelo = $user_agent;
-        break;
-    }
-}
-if ($mobile){
-    echo "Acesso feito via ".strtolower($modelo);
-}else{
-    echo "Acesso feito via computador";
-}
-?>
-		</script>
-
 @stop
